167fff456d922797bef5d3cdee2603c021735f3f
xyresh <81033344+xyresh@users.noreply.github.com>
2021-03-30 21:58:59 +0300

    Add files via upload

---
+++ numdesc.py
@@ -0,0 +1,63 @@
+#program to prove that four is the only number where its name "FOUR"
+#actually describes it's name
+#hypothesis by Kostas max and the code written by stackoverflow and Marios Par
+#proven correct byt the following simple python program
+
+import sys
+ 
+ 
+# Function to convert a single-digit or two-digit number into words
+def convertToDigit(n, suffix):
+ 
+    # if `n` is zero
+    if n == 0:
+        return EMPTY
+ 
+    # split `n` if it is more than 19
+    if n > 19:
+        return Y[n // 10] + X[n % 10] + suffix
+    else:
+        return X[n] + suffix
+ 
+ 
+# Function to convert a given number (max 9-digits) into words
+def convert(n):
+ 
+    # add digits at thousand and tens thousand place
+    result += convertToDigit(((n // 1000) % 100), "Thousand")
+ 
+    # add digit at hundred place
+    result += convertToDigit(((n // 100) % 10), "Hundred")
+ 
+    if n > 100 and n % 100:
+        result += "and "
+ 
+    # add digits at ones and tens place
+    result += convertToDigit((n % 100), "")
+ 
+    return result
+ 
+#main is here LOL 
+if __name__ == '__main__':
+ 
+    EMPTY = ""
+ 
+    X = [EMPTY, "One", "Two", "Three", "Four", "Five", "Six",
+        "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve",
+        "Thirteen", "Fourteen", "Fifteen", "Sixteen",
+        "Seventeen", "Eighteen", "Nineteen"]
+ 
+    Y = [EMPTY, EMPTY, "Twenty", "Thirty", "Forty", "Fifty",
+        "Sixty", "Seventy", "Eighty", "Ninety"]
+
+    numDesc = []
+
+    for i in range(99):
+        numString = convert(i)
+        if i == len(numString):
+            numDesc.append(i)
+
+        
+
+    print(numDesc)
+
